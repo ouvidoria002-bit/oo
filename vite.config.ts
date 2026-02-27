e17fb5b131b934ae6df1ea369905a38456b3a281
SrTesch <pedrotesch.pt@gmail.com>
2026-02-27 12:01:26 -0300

    :sparkles: adicionando kmls das paradas

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/kml-exports': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const baseUrl = env.VITE_BASE_URL || 'http://localhost:3004';
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: baseUrl,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/kml-exports': {
+          target: baseUrl,
+          changeOrigin: true,
+        }
       }
     }
   }
